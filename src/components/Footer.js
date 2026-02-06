dc6d75dccad0926e4955299c26c3ca288a0b1119
trojan9 <timmybanjo@gmail.com>
2026-02-06 23:44:48 +0000

    gix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -35,7 +35,7 @@ const Footer = () => {
         </div>
 
         <div className="footer-bottom">
-          <p>&copy; 2024 GiglarConcept. All rights reserved.</p>
+          <p>&copy; 2026 GiglarConcept. All rights reserved.</p>
         </div>
       </div>
     </footer>
